e1f7d3911996846c969d8781ca3200c218bcba1d
deejaygraham <deejaygraham@gmail.com>
2014-04-17 13:38:07 +0100

    Added build scripts
    Manual build via command line and travis script.

---
+++ Build.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+SET MsBuildPath="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+
+rem Is this a 64 bit machine?
+IF EXIST "%Programfiles(x86)%" SET MsBuildPath="%windir%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
+
+if "%1" == "" goto DefaultTarget
+
+goto SpecificTarget
+
+:DefaultTarget
+%MsBuildPath% NDifference.proj 
+goto End
+
+:SpecificTarget
+%MsBuildPath% NDifference.proj /t:%* 
+
+:End

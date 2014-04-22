1dad709b867b9efe36af27d0efe658c9bd1c8729
deejaygraham <deejaygraham@gmail.com>
2014-04-22 12:32:54 +0100

    Added shared version info

---
+++ src/CommonVersionInfo.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("")]
+
+#if DEBUG 
+[assembly: AssemblyConfiguration("Debug")] 
+#else 
+[assembly: AssemblyConfiguration("Release")] 
+#endif 
+
+[assembly: AssemblyCopyright("Copyright © 2011-2014")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0.0")] 
+[assembly: AssemblyProduct("NDifference")]
+[assembly: AssemblyTrademark("")]
+
+// All assemblies should have matching versions
+[assembly: AssemblyVersion("0.1.0.0")]
+
+[assembly: ComVisible(false)]

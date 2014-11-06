a5b578ea16ff2e1f695c6eac88e536af5292a20a
Derek Graham <deejaygraham@gmail.com>
2014-11-06 11:31:45 +0000

    Missed from last commit

---
+++ src/Core/Reporting/CssVariableRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDifference.Reporting
+{
+	public class CssVariableRepository
+	{
+		Dictionary<string, string> variables = new Dictionary<string, string>();
+
+		public void Declare(string variableName, string value)
+		{
+			string key = FormatAsVariable(variableName);
+
+			if (!this.variables.ContainsKey(key))
+			{
+				this.variables.Add(key, value);
+			}
+			else
+			{
+				variables[key] = value;
+			}
+		}
+		
+		public string Process(string text)
+		{
+			var styleBuilder = new StringBuilder(text);
+
+			foreach (string key in variables.Keys)
+			{
+				styleBuilder.Replace(key, variables[key]);
+			}
+
+			return styleBuilder.ToString();
+		}
+		
+		private string FormatAsVariable(string key)
+		{
+			string prefix = "$(";
+			string suffix = ")";
+
+			if (!key.StartsWith(prefix) && !key.EndsWith(suffix))
+				return String.Format("{0}{1}{2}", prefix, key, suffix);
+
+			return key; 
+		}
+	}
+
+
+}

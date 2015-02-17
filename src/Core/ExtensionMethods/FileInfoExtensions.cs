4794577b600c8af8e4a143a082804993f6720467
Derek Graham <deejaygraham@gmail.com>
2015-02-17 10:57:13 +0000

    Added extension to checksum calculation
    Defaults to md5

--- src/Core/ExtensionMethods/FileInfoExtensions.cs
+++ src/Core/ExtensionMethods/FileInfoExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Globalization;
 using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace NDifference
@@ -8,23 +9,29 @@ namespace NDifference
 	{
 		public static string CalculateChecksum(this FileSystemInfo info)
 		{
+			using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+			{
+				return info.CalculateChecksum(md5);
+			}
+		}
+
+		public static string CalculateChecksum(this FileSystemInfo info, HashAlgorithm hashAlgorithm)
+		{
 			const string HexFormat = "X2";
 			var builder = new StringBuilder();
 
 			using (FileStream fs = new FileStream(info.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
 			{
-				using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
-				{
-					byte[] hash = md5.ComputeHash(fs);
+				byte[] hash = hashAlgorithm.ComputeHash(fs);
 
-					for (int i = 0; i < hash.Length; ++i)
-					{
-						builder.Append(hash[i].ToString(HexFormat, CultureInfo.InvariantCulture));
-					}
+				for (int i = 0; i < hash.Length; ++i)
+				{
+					builder.Append(hash[i].ToString(HexFormat, CultureInfo.InvariantCulture));
 				}
 			}
 
 			return builder.ToString();
 		}
+
 	}
 }

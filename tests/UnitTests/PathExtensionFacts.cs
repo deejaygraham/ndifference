65aa527782acb1a5c5f09e2a92d305bafb505137
Derek Graham <deejaygraham@gmail.com>
2015-03-10 13:16:23 +0000

    Attempting to make path tests platform agnostic

--- tests/UnitTests/PathExtensionFacts.cs
+++ tests/UnitTests/PathExtensionFacts.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,52 +8,57 @@ using Xunit;
 
 namespace NDifference.UnitTests
 {
-	public class PathExtensionFacts
-	{
-		[Fact]
-		public void PathExtensions_Folder_Missing_Slash_Is_Accepted()
-		{
-			string baseFolder = "C:\\MyDocuments\\";
-			string absolutePath = "C:\\MyDocuments\\Hello.world";
+    public class PathExtensionFacts
+    {
+        [Fact]
+        public void PathExtensions_Folder_Missing_Slash_Is_Accepted()
+        {
+            string baseFolder = string.Format("C:{0}MyDocuments{0}", Path.DirectorySeparatorChar);
+            string absolutePath = string.Format("C:{0}MyDocuments{0}Hello.world", Path.DirectorySeparatorChar);
 
-			Assert.Equal("Hello.world", baseFolder.MakeRelativePath(absolutePath));
-		}
+            string expectedRelativePath = "Hello.world";
+            Assert.Equal(expectedRelativePath, baseFolder.MakeRelativePath(absolutePath));
+        }
 
-		[Fact]
-		public void PathExtensions_File_In_Folder_Is_Relative_To_Folder()
-		{
-			string baseFolder = "C:\\MyDocuments\\";
-			string absolutePath = "C:\\MyDocuments\\Hello.world";
+        [Fact]
+        public void PathExtensions_File_In_Folder_Is_Relative_To_Folder()
+        {
+            string baseFolder = string.Format("C:{0}MyDocuments{0}", Path.DirectorySeparatorChar);
+            string absolutePath = string.Format("C:{0}MyDocuments{0}Hello.world", Path.DirectorySeparatorChar);
 
-			Assert.Equal("Hello.world", baseFolder.MakeRelativePath(absolutePath));
-		}
+            string expectedRelativePath = "Hello.world";
+            Assert.Equal(expectedRelativePath, baseFolder.MakeRelativePath(absolutePath));
+        }
 
-		[Fact]
-		public void PathExtensions_File_In_SubFolder_Is_Relative_To_Folder()
-		{
-			string baseFolder = "C:\\MyDocuments\\";
-			string absolutePath = "C:\\MyDocuments\\Hello\\World.txt";
+        [Fact]
+        public void PathExtensions_File_In_SubFolder_Is_Relative_To_Folder()
+        {
+            string baseFolder = string.Format("C:{0}MyDocuments{0}", Path.DirectorySeparatorChar);
+            string absolutePath = string.Format("C:{0}MyDocuments{0}Hello{0}World.txt", Path.DirectorySeparatorChar);
 
-			Assert.Equal("Hello\\World.txt", baseFolder.MakeRelativePath(absolutePath));
-		}
+            string expectedRelativePath = string.Format("Hello{0}World.txt", Path.DirectorySeparatorChar);
+            Assert.Equal(expectedRelativePath, baseFolder.MakeRelativePath(absolutePath));
+        }
 
-		[Fact]
-		public void PathExtensions_File_In_Different_Child_Folder_Is_Relative_To_Folder()
-		{
-			string baseFolder = "C:\\MyDocuments\\World\\";
-			string absolutePath = "C:\\MyDocuments\\Hello\\World.txt";
+        [Fact]
+        public void PathExtensions_File_In_Different_Child_Folder_Is_Relative_To_Folder()
+        {
+            string baseFolder = string.Format("C:{0}MyDocuments{0}World{0}", Path.DirectorySeparatorChar);
+            string absolutePath = string.Format("C:{0}MyDocuments{0}Hello{0}World.txt", Path.DirectorySeparatorChar);
 
-			Assert.Equal("..\\Hello\\World.txt", baseFolder.MakeRelativePath(absolutePath));
-		}
+            string expectedRelativePath = string.Format("..{0}Hello{0}World.txt", Path.DirectorySeparatorChar);
+            Assert.Equal(expectedRelativePath, baseFolder.MakeRelativePath(absolutePath));
+        }
 
-		[Fact]
-		public void PathExtensions_File_In_Parent_Folder_Is_Relative_To_Child()
-		{
-			string baseFolder = "C:\\MyDocuments\\Summary.txt";
-			string absolutePath = "C:\\MyDocuments\\Hello\\World.txt";
+        [Fact]
+        public void PathExtensions_File_In_Parent_Folder_Is_Relative_To_Child()
+        {
+            string baseFolder = string.Format("C:{0}MyDocuments{0}Summary.txt", Path.DirectorySeparatorChar);
+            string absolutePath = string.Format("C:{0}MyDocuments{0}Hello{0}World.txt", Path.DirectorySeparatorChar);
 
-			Assert.Equal("..\\Summary.txt", absolutePath.MakeRelativePath(baseFolder));
-		}
+            string expectedRelativePath = string.Format("..{0}Summary.txt", Path.DirectorySeparatorChar);
 
-	}
+            Assert.Equal(expectedRelativePath, absolutePath.MakeRelativePath(baseFolder));
+        }
+    }
 }

3c2544cdccac249c33e2c0255de1956d47921316
Derek Graham <deejaygraham@gmail.com>
2018-08-14 10:35:33 +0100

    new unit test

---
+++ tests/UnitTests/Reporting/MethodCodeAsHtmlFacts.cs
@@ -0,0 +1,32 @@
+﻿using NDifference.Reporting;
+using NDifference.SourceFormatting;
+using NDifference.TypeSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NDifference.UnitTests
+{
+    public class MethodCodeAsHtmlFacts
+    {
+        [Fact]
+        public void Method_With_Generic_Return_Type_Is_Escaped()
+        {
+            var html4 = new ReportAsHtml4();
+
+            var mm = new MemberMethod();
+            mm.IsStatic = true;
+            mm.Signature = new Signature("ConvertToMetadata");
+            mm.ReturnType = new FullyQualifiedName("System.Collections.Generic.IDictionary<String,String>");
+
+            var tn = mm.ToCode();
+
+            string formatted = html4.Format(tn);
+
+            Assert.Contains("IDictionary&lt;String,String&gt;", formatted);
+        }
+    }
+}

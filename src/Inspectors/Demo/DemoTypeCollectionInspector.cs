9c22451f6cad6c226e765a7f062b07e756523499
Derek Graham <deejaygraham@gmail.com>
2024-06-17 11:05:25 +0100

    Update DemoTypeCollectionInspector.cs

--- src/Inspectors/Demo/DemoTypeCollectionInspector.cs
+++ src/Inspectors/Demo/DemoTypeCollectionInspector.cs
@@ -10,6 +10,7 @@ namespace NDifference.Inspectors
     /// </summary>
     public class DemoTypeCollectionInspector : ITypeCollectionInspector
 	{
+ // always disabled. Enable here for testing.
 		public bool Enabled { get { return false; } set { } }
 
 		public string ShortCode { get { return "TCI003"; } }

baa83c6699f44fb5bc127aefe69f2c53823de412
Derek Graham <deejaygraham@gmail.com>
2024-06-21 14:50:12 +0100

    Update OnTheFlyCompiler.cs

--- tests/UnitTests/OnTheFlyCompiler.cs
+++ tests/UnitTests/OnTheFlyCompiler.cs
@@ -1,74 +1,63 @@
-﻿using Microsoft.CSharp;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CSharp;
 using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 
 namespace NDifference.UnitTests
 {
-	/// <summary>
-	/// Compiles code into an assembly to allow reflection on test code.
-	/// </summary>
-	/// <example>
-	/// using (var ofc = new OnTheFlyCompiler())
-	/// {
-	///		Assembly assembly = ofc.Compile(code);
-	///		
-	///		...stuff...
-	///		
-	/// }
-	/// </example>
-	public sealed class OnTheFlyCompiler : IDisposable
+    /// <summary>
+    /// Compiles code into an assembly to allow reflection on test code.
+    /// </summary>
+    /// <example>
+    /// var ofc = new OnTheFlyCompiler();
+    /// {
+    ///		Assembly assembly = ofc.Compile(code);
+    ///		
+    ///		...stuff...
+    ///		
+    /// }
+    /// </example>
+    public sealed class OnTheFlyCompiler
 	{
 		private const string DllExtension = ".dll";
 		private const string PdbExtension = ".pdb";
 
-        private AssemblyDiskInfoBuilder infoBuilder = new AssemblyDiskInfoBuilder();
-
-		public OnTheFlyCompiler(CodeDomProvider provider, CompilerParameters parameters)
-		{
-			this.FileName = System.IO.Path.Combine(this.Folder, System.IO.Path.GetRandomFileName() + DllExtension);
-
-			this.Provider = provider;
-			this.Parameters = parameters;
-
-			this.References = new HashSet<string>(new string[] { "System.dll" });
-
-			parameters.OutputAssembly = this.FileName;
-		}
-
 		public OnTheFlyCompiler()
-			: this(new CSharpCodeProvider(
-				new Dictionary<string, string> { { "CompilerVersion", "v4.0" } }),
-				new CompilerParameters { GenerateExecutable = false, TreatWarningsAsErrors = false, WarningLevel = 4, IncludeDebugInformation = true })
 		{
+			//this.FileName = System.IO.Path.Combine(this.Folder, System.IO.Path.GetRandomFileName() + DllExtension);
+            this.FileName = System.IO.Path.GetRandomFileName();
+            var codeBase = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var localPath = new Uri(codeBase).LocalPath;
+
+            this.Folder = Path.Combine(System.IO.Path.GetDirectoryName(localPath), System.IO.Path.GetRandomFileName());
+
+            this.References = new HashSet<string>(new string[]
+            {
+                typeof(System.Object).GetTypeInfo().Assembly.Location,
+                Path.Combine(Path.GetDirectoryName(typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly.Location), "System.Runtime.dll")
+            });
 		}
-
-		public bool OptimiseCode { get; set; }
-
-		public bool Targetx64 { get; set; }
-
-		public string CompilerVersion { get; set; }
 
 		public HashSet<string> References { get; private set; }
 
-		private CodeDomProvider Provider { get; set; }
+		private string Folder { get; set; }
 
-		private CompilerParameters Parameters { get; set; }
+		public string FileName { get; set; }
 
-		private string Folder
-		{
-			get
-			{
-				var codeBase = System.Reflection.Assembly.GetExecutingAssembly().Location;
-				var localPath = new Uri(codeBase).LocalPath;
-
-				return System.IO.Path.GetDirectoryName(localPath);
-			}
-		}
-
-		private string FileName { get; set; }
+        private string FullPath
+        {
+            get
+            {
+                return System.IO.Path.Combine(this.Folder, this.FileName + DllExtension);
+            }
+        }
 
 		public IAssemblyDiskInfo Compile(IBuildToCode code)
 		{
@@ -77,52 +66,59 @@ namespace NDifference.UnitTests
 
 		public IAssemblyDiskInfo Compile(string code)
 		{
-			Debug.Assert(this.Provider != null, "CodeDomProvider is not set");
-			Debug.Assert(this.Parameters != null, "CompilerParameters not set");
 			Debug.Assert(!string.IsNullOrEmpty(code), "Code cannot be blank");
 
-			string compilerOptions = string.Empty;
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(code);
 
-			if (this.OptimiseCode)
-				compilerOptions += "/optimize+ ";
+            MetadataReference[] references = this.References.Select(r => MetadataReference.CreateFromFile(r)).ToArray();
 
-			if (this.Targetx64)
-				compilerOptions += "/platform:x64";
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                this.FileName,
+                syntaxTrees: new[] { syntaxTree },
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
 
-			this.Parameters.CompilerOptions = compilerOptions;
-			this.Parameters.ReferencedAssemblies.AddRange(this.References.ToArray());
+            using (var ms = new MemoryStream())
+            {
+                EmitResult result = compilation.Emit(ms);
 
-			var results = this.Provider.CompileAssemblyFromSource(this.Parameters, code);
+                if (result.Success)
+                {
+                    // save to disk 
+                    string tempFolder = System.IO.Path.GetDirectoryName(this.FullPath);
 
-			if (results.Errors.HasErrors)
-			{
-				throw new InvalidOperationException("Compilation Failed:\n" + string.Join(Environment.NewLine, results.Errors.BuildErrorMessageList(code).ToArray()));
-			}
+                    if (!Directory.Exists(tempFolder))
+                        Directory.CreateDirectory(tempFolder);
 
-			return infoBuilder.BuildFromFile(this.FileName);
-		}
+                    using (Stream assembly = File.Open(this.FullPath, FileMode.Create))
+                    {
+                        ms.Seek(0, SeekOrigin.Begin);
+                        ms.CopyTo(assembly);
 
-		public void Dispose()
-		{
-			if (this.Provider != null)
-			{
-				this.Provider.Dispose();
-				this.Provider = null;
-			}
+                        assembly.Close();
+                    }
 
-			this.FileName.DeleteIfExists();
-			string debugDBName = this.FileName.GetCompanionFile(PdbExtension);
-			debugDBName.DeleteIfExists();
+                    AssemblyDiskInfoBuilder infoBuilder = new AssemblyDiskInfoBuilder();
+                    return infoBuilder.BuildFromFile(this.FullPath);
+                }
+
+                IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                    diagnostic.IsWarningAsError ||
+                    diagnostic.Severity == DiagnosticSeverity.Error);
+
+                string message = string.Join(Environment.NewLine, failures.BuildErrorMessageList(code).ToArray());
+                throw new InvalidOperationException("Compilation Failed:\n" + message);
+            }
 		}
 	}
 
 	public static class CompilerErrorExtensions
 	{
-		public static IEnumerable<string> BuildErrorMessageList(this CompilerErrorCollection allErrors, string code)
+		public static IEnumerable<string> BuildErrorMessageList(this IEnumerable<Diagnostic> allErrors, string code)
 		{
 			var errors = new List<string>();
 
-			foreach (CompilerError compilerError in allErrors)
+			foreach (Diagnostic compilerError in allErrors)
 			{
 				errors.Add(compilerError.ToDevStudioFormat());
 			}
@@ -133,9 +129,9 @@ namespace NDifference.UnitTests
 			return errors;
 		}
 
-		public static string ToDevStudioFormat(this CompilerError error)
+		public static string ToDevStudioFormat(this Diagnostic error)
 		{
-			return string.Format("{0}({1},{2}): error {3}: {4}", error.FileName, error.Line, error.Column, error.ErrorNumber, error.ErrorText);
+			return string.Format("{0}: {1}", error.Id, error.GetMessage());
 		}
 	}
 }

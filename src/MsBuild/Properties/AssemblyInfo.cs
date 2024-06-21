c8df0ae2931caacfad6633034329e1494ba0fc4b
Derek Graham <deejaygraham@gmail.com>
2024-06-21 11:40:26 +0100

    Delete src/MsBuild directory

--- src/MsBuild/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NDifference.Tasks")]
-[assembly: AssemblyDescription("MsBuild tasks for NDifference")]
-[assembly: AssemblyCulture("")]
